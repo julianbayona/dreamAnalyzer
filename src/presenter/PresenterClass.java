29386c2419d74b785f37fa7c54e5065cb3b4343b
Yesid Martinez <yesid.martinez04@uptc.edu.co>
2025-04-06 23:06:33 -0500

    view

--- src/presenter/PresenterClass.java
+++ src/presenter/PresenterClass.java
@@ -1,17 +1,88 @@
 package presenter;
 
+import model.Manager;
 import view.ViewClass;
 
 public class PresenterClass {
     private ViewClass view;
+    private Manager manager;
 
     public PresenterClass() {
         view = new ViewClass();
-        this.inicioSecion();
+        manager = new Manager();
+        this.starSession();
     }
 
-    public void inicioSecion(){
-        view.showMenuSession();
+    public void starSession(){
+        Boolean exit=false;
+        
+        while (exit==false){
+            String session = view.showMenuSession();
+            if (session.equals("Therapist")){
+                sessionTherapist();
+            }else if (session.equals("Patient")){
+                sessionPatient();
+                
+            }else if (session.equals("exit")){
+                exit=true;
+                view.exitMessage();
+            }else {
+                System.out.println("Opción no válida, intente de nuevo.");
+            }   
+        }
+    }
+
+    public void sessionTherapist(){
+        Boolean exit=false;
+        while (exit==false){
+            view.showMenuTherapist();
+            String option = view.getInput("Seleccione la opcion a realizar");
+            switch (option) {
+                case "1":
+                    // Analizar sueño (recuerden cambar segun la logica)
+                    manager.analizeDream(option, option);
+                    break;
+                case "2":
+                    // Duplicar y experimentar con un sueño
+                    //Agregar la logica de duplicar y experimentar con un sueño del manager
+                    break;
+                case "3":
+                    // Generar informe de un sueño (recuerden cambar segun la logica)
+                    manager.createReport(option);
+                    break;
+                case "4":
+                    // Configure System (recuerden cambar segun la logica)
+                    manager.systemConfig();
+                    break;
+                case "5":
+                    // exit
+                    view.exitMessage();
+                    exit=true;
+                    break;
+                default:
+                    System.out.println("Opción no válida, intente de nuevo.");
+            }
+        }
     }
     
+    public void sessionPatient(){
+        Boolean exit=false;
+        while (exit==false){
+            view.showMenuPatient();
+            String option = view.getInput("Seleccione la opcion a realizar");
+            switch (option) {
+                case "1":
+                    // Registrar sueño (recuerden cambar segun la logica)
+                    manager.regysterDream(option);
+                    break;
+                case "2":
+                    // exit
+                    view.exitMessage();
+                    exit=true;
+                    break;
+                default:
+                    System.out.println("Opción no válida, intente de nuevo.");
+            }
+        }
+    }
 }
--- src/view/ViewClass.java
+++ src/view/ViewClass.java
@@ -10,12 +10,29 @@ public class ViewClass {
     }
 
     public String showMenuSession() {
-        System.out.println("Bienvenido al sistema de análisis de sueños, como queiere iniciar sesión");
-        System.out.println("1. Usuario");
-        System.out.println("2. Administrador");
-        System.out.println("3. Salir");
-        System.out.print("Seleccione una opción: ");
-        return scanner.next();
+        Boolean sesion =false;
+        String option = "";
+        while(sesion==false){
+            System.out.println("\nBienvenido al sistema de análisis de sueños, como queiere iniciar sesión");
+            System.out.println("1. Terapeuta");
+            System.out.println("2. Paciente");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            option = scanner.nextLine();
+            if (option.equals("1")) {
+                option = "Therapist";
+                sesion=true;
+            } else if (option.equals("2")) {
+                option = "Patient";
+                sesion=true;
+            } else if (option.equals("3")) {
+                option = "exit";
+                sesion=true;
+            } else {
+                System.out.println("Opción no válida, intente de nuevo.");
+            }
+        }
+        return option;
     }
 
     public String getInput(String prompt) {
@@ -23,12 +40,20 @@ public class ViewClass {
         return new java.util.Scanner(System.in).nextLine();
     }
 
-    public void showMenu() {
-        System.out.println("1. Create Repository");
-        System.out.println("2. Register Dream");
-        System.out.println("3. Analyze Dream");
-        System.out.println("4. Configure System");
-        System.out.println("5. Create Report");
-        System.out.println("6. Exit");
+    public void showMenuTherapist() {
+        System.out.println("1. Analizar Sueño");
+        System.out.println("2. Duplicar y experimentar con un sueño");
+        System.out.println("3. Generar informe de un sueño");
+        System.out.println("4. Configurar sistema");
+        System.out.println("5. Exit");
+    }
+
+    public void showMenuPatient() {
+        System.out.println("1. Registrar sueño");
+        System.out.println("2. Exit");
+    }
+
+    public void exitMessage(){
+        System.out.println("Sesion cerrada, gracias por usar el sistema de analisis de sueños");
     }
 }
